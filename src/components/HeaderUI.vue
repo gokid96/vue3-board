ab2965ddbff35aa4218f067f6d2d404e778d719f
sungminko <gokid@nate.com>
2025-05-28 17:53:24 +0900

    pushfist

---
+++ src/components/HeaderUI.vue
@@ -0,0 +1,16 @@
+<template>
+
+  <nav class="container  mx-auto flex justify-between items-center">
+    <div class="text-2xl font-bold">Myboard</div>
+    <ul class="flex space-x-4">
+
+    </ul>
+  </nav>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
